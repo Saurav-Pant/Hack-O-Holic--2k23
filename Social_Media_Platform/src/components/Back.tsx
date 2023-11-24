752b888af4437224fa4db9f69c2e731020050660
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-11-24 21:46:47 +0530

    back and footer modified

--- Social_Media_Platform/src/components/Back.tsx
+++ Social_Media_Platform/src/components/Back.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Back = (props: Props) => {
   return (
     <>
-      <button className="bg-white  text-black absolute top-10 left-10 px-4 py-2 rounded-lg ">
+      <button className="bg-gradient-to-b from-gray-700 via-gray-900 to-black  text-white absolute top-10 left-10 px-4 py-2 rounded-lg ">
         <Link href={"/"}>Back</Link>
       </button>
     </>
