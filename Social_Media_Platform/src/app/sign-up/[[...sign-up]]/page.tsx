ddda0d74c34340beafb6201777f7fd1aebc43ce6
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-11-24 17:13:05 +0530

    footer added

--- Social_Media_Platform/src/app/sign-up/[[...sign-up]]/page.tsx
+++ Social_Media_Platform/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import Back from "@/components/Back";
 import { SignUp } from "@clerk/nextjs";
-
+import Footer from "../../../components/Footer";
 export default function Page() {
   return (
     <div>
@@ -8,6 +8,8 @@ export default function Page() {
       <div className="flex justify-center items-center h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-400 via-gray-600 to-gray-800">
         <SignUp />;
       </div>
+      <Footer/>
+
     </div>
   );
 }
