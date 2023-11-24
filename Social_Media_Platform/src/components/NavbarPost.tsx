89251fd4bfe287b92a019ef4cbed419fea99fee8
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-11-24 22:39:09 +0530

    feed photos changed

--- Social_Media_Platform/src/components/NavbarPost.tsx
+++ Social_Media_Platform/src/components/NavbarPost.tsx
@@ -7,7 +7,7 @@ const NavbarPost = () => {
   return (
     <div className="bg-gradient-to-b from-gray-700 via-gray-900 to-black w-full p-4 flex items-center text-white border-b-4 border-gray-300 fixed -mt-5">
       <div className="text-2xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
-        friendsBook
+        FriendsBook
       </div>
       <div className="flex-grow mx-4">
         <input
