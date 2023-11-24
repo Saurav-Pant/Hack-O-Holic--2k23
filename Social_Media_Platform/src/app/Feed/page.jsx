217f6bf8830e9abd25c8ec988a4886f13b9f4e0b
Saurav Pant <pantsaurav005@gmail.com>
2023-11-24 23:27:36 +0530

    added

--- Social_Media_Platform/src/app/Feed/page.jsx
+++ Social_Media_Platform/src/app/Feed/page.jsx
@@ -1,31 +1,28 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import posts from "../../content/posts.json";
 import NavbarPost from "@/components/NavbarPost";
 import { IoAddCircleOutline } from "react-icons/io5";
+import Link from "next/link";
 
 const Post = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // Filter posts based on user input
+  const [searchQuery, setSearchQuery] = useState("");
   const filteredPosts = posts.filter((post) =>
-    post.user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    post.user.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <>
       <NavbarPost />
-      <div className="flex flex-col justify-center items-center mt-5 ">
-        {/* Search Input */}
+      <div className="flex flex-col justify-center items-center mt-5">
         <input
           type="text"
           placeholder="Search by user name"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
           className="mb-3 px-4 py-2 border rounded-md"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
 
-        {/* Display filtered posts */}
         {filteredPosts.map((post) => (
           <div key={post.id} className="bg-slate-200 mb-5">
             <div className="flex justify-around items-center">
@@ -36,22 +33,19 @@ const Post = () => {
                   className="w-8 h-8 rounded-full inline-block mr-2"
                 />
                 <span className="text-gray-700">{post.user.name}</span>
-              </div>
+              </div>l
               <div>
                 <button className="bg-red-400 px-5 py-2">Add Friend</button>
               </div>
             </div>
-            <div
-              key={post.id}
-              className="rounded overflow-hidden shadow-lg mb-10"
-            >
+            <div className="rounded overflow-hidden shadow-lg mb-10">
               <img
                 src={post.image}
                 alt={`Post by ${post.user.name}`}
                 className="h-96 w-full"
               />
             </div>
-            <div className="flex justify-around mb-5 border-t-2  border-b-2 border-gray-400">
+            <div className="flex justify-around mb-5 border-t-2 border-b-2 border-gray-400">
               <button className="border-solid ">Likes</button>
               <button>Comment</button>
               <hr />
@@ -60,9 +54,11 @@ const Post = () => {
         ))}
 
         <div className="fixed bottom-7 right-7">
-          <button>
-            <IoAddCircleOutline size={60} />
-          </button>
+          <Link href={"/upload"}>
+            <button>
+              <IoAddCircleOutline size={60} />
+            </button>
+          </Link>
         </div>
       </div>
     </>
--- Social_Media_Platform/src/components/NavbarPost.tsx
+++ Social_Media_Platform/src/components/NavbarPost.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import { IoHome } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
+import Link from "next/link";
 
 const NavbarPost = () => {
   return (
     <div className="bg-gradient-to-b from-gray-700 via-gray-900 to-black w-full p-4 flex items-center text-white border-b-4 border-gray-300 fixed -mt-5">
       <div className="text-2xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
-        friendsBook
+        <Link href={"/"}>
+        FriendsBook
+        </Link>
       </div>
       <div className="flex-grow mx-4">
         <input
@@ -17,9 +20,13 @@ const NavbarPost = () => {
         />
       </div>
       <div className="flex">
-        <div className="mx-10 cursor-pointer"> {<IoHome size={30}/>}</div>
-        <div className="mx-10 cursor-pointer"><FaUserFriends size={30} /></div>
-        <div className="mx-10 cursor-pointer">< AiFillMessage size={30} /></div>
+        <div className="mx-10 cursor-pointer"> {<IoHome size={30} />}</div>
+        <div className="mx-10 cursor-pointer">
+          <FaUserFriends size={30} />
+        </div>
+        <div className="mx-10 cursor-pointer">
+          <AiFillMessage size={30} />
+        </div>
       </div>
     </div>
   );
